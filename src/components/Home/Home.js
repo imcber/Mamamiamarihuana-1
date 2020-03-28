a3e01a40fc1eb1ba7e9275ed3285aa020a53f5f1
Imcber <imcber1@gmail.com>
2020-03-28 16:07:18 -0600

    trabajando con eventos y vistas

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import Header from "../Header/Header";
 
 import "./Home.scss";
 
+/**
+ * Display home view
+ */
 const Home = () => {
+  const [firstMove, setFirstMove] = useState(false);
+  const handlerFirstMove = () => {
+    setFirstMove(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handlerFirstMove, false);
+    return () => {
+      document.removeEventListener("keydown", handlerFirstMove, false);
+    };
+  }, [firstMove]);
+
   return (
     <Container maxWidth="xl">
       <Header />
       <div className="home-container">
-        <div className="home-container__text-container">
-          <span className="home-container__typewriter">
-            ¿Cual es el valor de vivir?
-          </span>
-        </div>
+        <TextDisplay classSpan="typewriter">
+          ¿Cual es el valor de vivir?
+        </TextDisplay>
+        {!firstMove && (
+          <TextDisplay classSpan="info-aux">
+            Presiona cualquier tecla...
+          </TextDisplay>
+        )}
       </div>
     </Container>
   );
 };
 
 export default Home;
+
+/**Text that change */
+const TextDisplay = props => {
+  const { classSpan, children } = props;
+  return (
+    <div className="home-container__text-container">
+      <span className={`home-container__${classSpan}`}>{children}</span>
+    </div>
+  );
+};
